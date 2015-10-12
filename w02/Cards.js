b9593237ce6412c55d1ae9d6e0aef3f007209c1e
radio! <radio.github@gmail.com>
2015-10-11 21:14:25 -0700

    Better format

--- w02/Cards.js
+++ w02/Cards.js
@@ -1,88 +1,115 @@
-'use strict';
-var card = function (rank, suit) {
+/*jslint indent: 4, maxerr: 50, vars: true */
 
-	this.rank = function () {
-		return rank;
-	};
+// FG - CS454
 
-	this.suit = function () {
-		return suit;
-	};
+var card, deck;
 
-	this.value = function () {
-		if (rank === 'Ace') {
-			return 1;
-		}
+(function () {
 
-		if (rank === 'Jack' || rank === 'Queen' || rank === 'King') {
-			return 10;
-		}
+	'use strict';
 
-		return rank;
-	};
+	// uppercase constructors . . .
+	function Card(rank, suit) {
 
-};
+		this.rank = function () {
+			return rank;
+		};
 
-card.prototype.toString = function () {
-	return this.rank() + ' of ' + this.suit();
-};
+		this.suit = function () {
+			return suit;
+		};
 
-function deck() {
-	this.reset();
-}
+		this.value = function () {
+			if (rank === 'Ace') {
+				return 1;
+			}
 
-deck.prototype.deal = function () {
-	return this.cards.shift();
-};
+			if (rank === 'Jack' || rank === 'Queen' || rank === 'King') {
+				return 10;
+			}
 
-deck.prototype.remaining = function () {
-	return this.cards.length;
-};
+			return rank;
+		};
 
-deck.prototype.shuffle = function () {
-	var a = this.cards, i, len = a.length, r, temp;
-
-	for (i = len - 1; i > 0; i -= 1) {
-		r = Math.floor(Math.random() * i);
-
-		temp = a[i];
-		a[i] = a[r];
-		a[r] = temp;
 	}
 
-	return a;
-};
+	Card.prototype.toString = function () {
+		return this.rank() + ' of ' + this.suit();
+	};
 
-deck.prototype.reset = function () {
-	var ranks = ['Ace', 2, 3, 4, 5, 6, 7, 8, 9, 10, 'Jack', 'Queen', 'King'],
-		suits = ['Clubs', 'Diamonds', 'Hearts', 'Spades'],
-		cards = [],
-		i,
-		j;
+	var card1 = new Card('Ace', 'Spades'); // Creates the Ace of Spades Card
+	var card2 = new Card('9', 'Hearts'); // Creates the 9 of Hearts Card
 
-	for (i = 0; i < ranks.length; i += 1) {
-		for (j = 0; j < suits.length; j += 1) {
-			cards.push(new card(ranks[i], suits[j]));
-		}
+	console.log(card1.value()); // Prints 1
+	console.log('Card1 is: ' + card1); // Ace of Spades
+	console.log(card2.value()); // Prints 9
+	console.log('Card2 is: ' + card2); // 9 of Hearts
+
+	function Deck() {
+		this.reset();
 	}
 
-	this.cards = cards;
-};
+	Deck.prototype.deal = function () {
+		return this.cards.shift();
+	};
 
-var myDeck = new deck();
-myDeck.shuffle();
-var hand1 = [];
-hand1.push(myDeck.deal());
-hand1.push(myDeck.deal());
-// Note, the output below will vary since the deck will be shuffled
-console.log('Hand 1: ', hand1[0], hand1[1]); // Hand 1: 2 of Hearts 9 of Clubs
-var hand2 = [];
-hand2.push(myDeck.deal());
-hand2.push(myDeck.deal());
-console.log('Hand 2: ', hand2[0], hand2[1]); // Hand 2: Queen of Diamonds Jack of Clubs
-console.log('The value of Hand 1 is: ', hand1[0].value() + hand1[1].value());
-console.log('The value of Hand 2 is: ', hand2[0].value() + hand2[1].value());
-console.log('There are ', myDeck.remaining(), ' cards remaining in the deck');
-//48
-myDeck.reset();
-console.log('There are ', myDeck.remaining(), ' cards remaining in the deck');
+	Deck.prototype.remaining = function () {
+		return this.cards.length;
+	};
+
+	Deck.prototype.shuffle = function () {
+		var a = this.cards, i, len = a.length, r, temp;
+
+		for (i = len - 1; i > 0; i -= 1) {
+			r = Math.floor(Math.random() * i);
+
+			temp = a[i];
+			a[i] = a[r];
+			a[r] = temp;
+		}
+
+		return a;
+	};
+
+	Deck.prototype.reset = function () {
+		var ranks = ['Ace', 2, 3, 4, 5, 6, 7, 8, 9, 10, 'Jack', 'Queen', 'King'],
+			suits = ['Clubs', 'Diamonds', 'Hearts', 'Spades'],
+			cards = [],
+			i,
+			j;
+
+		for (i = 0; i < ranks.length; i += 1) {
+			for (j = 0; j < suits.length; j += 1) {
+				cards.push(new Card(ranks[i], suits[j]));
+			}
+		}
+
+		this.cards = cards;
+	};
+
+	var myDeck = new Deck();
+	myDeck.shuffle();
+
+	var hand1 = [];
+	hand1.push(myDeck.deal());
+	hand1.push(myDeck.deal());
+
+	// Note, the output below will vary since the Deck will be shuffled
+	console.log('Hand 1: ' + hand1[0] + ' ' + hand1[1]); // Hand 1: 2 of Hearts 9 of Clubs
+
+	var hand2 = [];
+	hand2.push(myDeck.deal());
+	hand2.push(myDeck.deal());
+
+	console.log('Hand 2: ' + hand2[0] + ' ' + hand2[1]); // Hand 2: Queen of Diamonds Jack of Clubs
+	console.log('The value of Hand 1 is: ', hand1[0].value() + hand1[1].value());
+	console.log('The value of Hand 2 is: ', hand2[0].value() + hand2[1].value());
+
+	console.log('There are ', myDeck.remaining(), ' cards remaining in the Deck'); // 48
+	myDeck.reset();
+	console.log('There are ', myDeck.remaining(), ' cards remaining in the Deck'); // 52
+
+	card = Card;
+	deck = Deck;
+
+}());
